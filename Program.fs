3d1b0503a228fe7618d06c6b8a5476c0de66840a
Ben Follington <5009316+bfollington@users.noreply.github.com>
2020-07-13 09:47:35 +1000

    Handle DB Connection errors

--- Program.fs
+++ Program.fs
@@ -8,40 +8,47 @@ open Sketch.Commands
 let fancyHat = { Sku = "123"; Name = "Fancy Hat"; Price = 999.0M }
 let uglyHat = { Sku = "456"; Name = "Ugly Hat"; Price = 1.0M }
 
+let setup =
+    try
+        let conn = Connection.mkOnDisk() 
+        conn.Open()
+        Ok <| conn.BeginTransaction()
+    with
+    | e -> Error e
+
 [<EntryPoint>]
 let main argv =
-    let conn = Connection.mkOnDisk()
-    
-    conn.Open()
-    let txn = conn.BeginTransaction()
+    match setup with
+    | Error exn -> printfn "! Failed to connect to DB: %A" exn.Message
+    | Ok txn -> 
+        let conn = txn.Connection
+        let myCartId = 123
+        let cart = Cart.Client conn
 
-    let myCartId = 123
-    let cart = Cart.Client conn
+        let state = 
+            Cart.hydrate myCartId conn |> Ok
+            |> Result.bind(cart.CreateCart myCartId)
+            |> Result.bind(cart.AddItemToCart myCartId fancyHat)
+            |> Result.bind(cart.AddItemToCart myCartId uglyHat)
+            |> Result.bind(cart.RemoveItemFromCart myCartId fancyHat)
+            |> Result.bind(cart.CheckOut myCartId)
+            |> Result.bind(cart.Shipped myCartId)
 
-    let state = 
-        Cart.hydrate myCartId conn
-        |> Ok
-        |> Result.bind(cart.CreateCart myCartId)
-        |> Result.bind(cart.AddItemToCart myCartId fancyHat)
-        |> Result.bind(cart.AddItemToCart myCartId uglyHat)
-        |> Result.bind(cart.RemoveItemFromCart myCartId fancyHat)
-        |> Result.bind(cart.CheckOut myCartId)
-        |> Result.bind(cart.Shipped myCartId)
+        printfn "\n----\n"
 
-    printfn "\n----\n"
+        match state with
+        | Ok s -> 
+            printfn "Ok, state =\n\n %A" s
+            txn.Commit()
+        | Error s -> 
+            printfn "Rejected, error =\n\n %A" s
+            txn.Rollback()
 
-    match state with
-    | Ok s -> 
-        printfn "Ok, state =\n\n %A" s
-        txn.Commit()
-    | Error s -> 
-        printfn "Rejected, error =\n\n %A" s
-        txn.Rollback()
+        printfn "\n----\n"
 
-    printfn "\n----\n"
+        printfn "Loaded again, cart =\n\n %A" (Cart.hydrate myCartId conn)
 
-    printfn "Loaded again, cart =\n\n %A" (Cart.hydrate myCartId conn)
+        conn.Close()
 
-    conn.Close()
     printfn "Bye!"
     0 // return an integer exit code
